47019588b69492667c84f60328e62352bbf8a90d
maikytech <maikytech1307@gmail.com>
2018-12-05 16:37:14 -0500

    BFS finished for 4 vertices

--- BFSforAGraph.cpp
+++ BFSforAGraph.cpp
@@ -23,6 +23,33 @@ void Graph::BFS(int s)
     list<int> queue;
 
     //Mark the current node as visited and enqueue it.
+    //push_back inserts the element at the end of the list.
     visited[s] = true;
     queue.push_back(s);
+
+    //We get all adjacent vertices of a vertex
+    list<int>::iterator i;
+
+    while(!queue.empty())
+    {
+        //Dequeue a vertex from queue and print it.
+        //front() return a first element reference.
+        //pop_front removes the first item from the list.
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+        //i is first of the list, till i is not the last...
+        //begin() return a random access iterator that points to the begining of the list.
+        //end() return a random access iterator that points to the end of the list.
+        for(i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+
+        }
+    }
 }
---
+++ MainBFSforAGraph.cpp
@@ -0,0 +1,32 @@
+//
+//  MainBFSforAGraph.cpp
+//
+//  Created by Maiqui Cedeño on 12/4/18.
+//  Copyright © 2018 Maiqui Cedeño. All rights reserved.
+//
+// Program to print BFS traversal from a given source vertex.
+//
+
+#include <iostream>
+#include "BFSforAGraph.h"
+
+int main()
+{
+    //Create a Graph with 4 vertices
+
+    Graph g(4);
+
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(2,0);
+    g.addEdge(2,3);
+    g.addEdge(3,3);
+
+    cout << "Following is Breadth First Traversal"
+         << "(starting from vertex 2) \n";
+
+    g.BFS(2);
+
+    return 0;
+}
